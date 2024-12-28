234862f48d52703b18a07f5c456fa34f8401d7cc
Alex McIver <alexmciver@ymail.com>
2024-12-28 16:06:07 +0000

    Setup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
 const terser = require('gulp-terser');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
@@ -9,24 +10,35 @@ const browserSync = require('browser-sync').create();
 // File paths
 const paths = {
     styles: {
-        src: 'css/**/*.css',
+        src: 'src/scss/**/*.scss',
         dest: 'dist/css'
     },
     scripts: {
-        src: 'js/**/*.js',
+        src: 'src/js/**/*.js',
         dest: 'dist/js'
     },
     html: {
-        src: '*.html'
+        src: 'public/**/*.html',
+        dest: 'dist'
+    },
+    assets: {
+        src: 'src/assets/**/*',
+        dest: 'dist/assets'
+    },
+    data: {
+        src: 'src/data/**/*',
+        dest: 'dist/data'
     }
 };
 
 // CSS task
 function styles() {
-    return gulp.src(paths.styles.src)
-        .pipe(concat('style.min.css'))
+    return gulp.src('src/scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(cleanCSS())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(paths.styles.dest))
         .pipe(browserSync.stream());
 }
