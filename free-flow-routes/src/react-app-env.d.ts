edb341c5462b5a3cd791480edb4daad893c08adc
Alex McIver <alexmciver@ymail.com>
2026-04-30 13:18:45 +0100

    Remove unused files and configurations as part of the transition to Next.js, including environment files, Jest configurations, and various components.

--- free-flow-routes/src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-/// <reference types="react-dom" />
-/// <reference types="next" />
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    [elemName: string]: any;
-  }
-} 
--- free-flow-routes/src/types/nodejs.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface Timeout {}
-  interface Process {
-    env: {
-      [key: string]: string | undefined;
-      NODE_ENV: 'development' | 'production' | 'test';
-    };
-  }
-} 
